2fee733baf26b91661922001a129a5f269f10261
Eoghan Mulvenna <venvenn@gmail.com>
2018-02-16 11:32:38 +0000

    Started work on level data system

---
+++ TrainsMission/Assets/Scripts/LevelData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    public Level CurrentLevel;
+    public List<Level> LevelList = new List<Level>();
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+
+}
+
+//level data structure
+[System.Serializable]
+public class Level
+{
+    public GameMode mode;
+    public string name;
+    public int mapSize;
+    public int stationNum;
+    public int lineNum;
+    public int stationsPerLine;
+}
+
+//map types
+[System.Serializable]
+public enum GameMode
+{
+    Campaign,
+    Tutorial
+}
